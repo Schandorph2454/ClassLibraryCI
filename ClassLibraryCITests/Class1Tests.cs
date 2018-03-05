b2e500baa722c34c7dc7c2719d9adc6a529cff4c
Linda Lene Schandorph Larsen <lind6181@edu.easj.dk>
2018-03-05 12:41:09 +0100

    Udført en unittest

---
+++ ClassLibraryCITests/Class1Tests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassLibraryCI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryCI.Tests
+{
+    [TestClass()]
+    public class Class1Tests
+    {
+        [TestMethod()]
+        public void GetNameTest()
+        {
+            //Arrange
+            Class1 myName = new Class1();
+            //Act
+            string hello = myName.GetName();
+            //Assert
+            Assert.AreEqual("Linda", hello);
+        }
+
+        [TestMethod()]
+        public void GetAddressTest()
+        {
+            //Arrange
+            Class1 myAddress = new Class1();
+            //Act
+            string elisagaardsvej = myAddress.GetAddress();
+            //Assert
+            Assert.AreEqual("Gartnervang", elisagaardsvej );
+        }
+    }
+}
